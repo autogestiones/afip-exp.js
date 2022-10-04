ffc3a154c98ab6ac7906b6cbfb22c7dd0b6dc93c
ramzony <ramirosacruz@gmail.com>
2022-10-04 10:10:32 -0300

    añadiendo clase ElectronicExportBilling y wrappeando las funciones de su API SOAP

--- src/Afip.js
+++ src/Afip.js
@@ -1,123 +1,129 @@
-const fs = require('fs');
-const path = require('path');
-const soap = require('soap');
-const forge = require('node-forge');
-const xml2js = require('xml2js');
+const fs = require("fs");
+const path = require("path");
+const soap = require("soap");
+const forge = require("node-forge");
+const xml2js = require("xml2js");
 
 // XML parser
 var xmlParser = new xml2js.Parser({
-	normalizeTags: true,
-	normalize: true,
-	explicitArray: false,
-	attrkey: 'header',
-	tagNameProcessors: [key => key.replace('soapenv:', '')]
+  normalizeTags: true,
+  normalize: true,
+  explicitArray: false,
+  attrkey: "header",
+  tagNameProcessors: [(key) => key.replace("soapenv:", "")],
 });
 
 // Generic Web Service
-const AfipWebService = require('./Class/AfipWebService');
-
+const AfipWebService = require("./Class/AfipWebService");
 
 // Available Web Services
-const ElectronicBilling = require('./Class/ElectronicBilling');
-const RegisterScopeFour = require('./Class/RegisterScopeFour');
-const RegisterScopeFive = require('./Class/RegisterScopeFive');
-const RegisterScopeTen = require('./Class/RegisterScopeTen');
-const RegisterScopeThirteen = require('./Class/RegisterScopeThirteen');
+const ElectronicBilling = require("./Class/ElectronicBilling");
+const ElectronicExportBilling = require("./Class/ElectronicExportBilling");
 
 /**
  * Software Development Kit for AFIP web services
- * 
- * This release of Afip SDK is intended to facilitate 
- * the integration to other different web services that 
- * Electronic Billing   
- * 
+ *
+ * This release of Afip SDK is intended to facilitate
+ * the integration to other different web services that
+ * Electronic Billing
+ *
  * @link http://www.afip.gob.ar/ws/ AFIP Web Services documentation
- * 
+ *
  * @author 	Afip SDK afipsdk@gmail.com
  * @package Afip
  * @version 0.6
  **/
 module.exports = Afip;
 
-function Afip(options = {}){
-	/**
-	 * File name for the WSDL corresponding to WSAA
-	 *
-	 * @var string
-	 **/
-	this.WSAA_WSDL;
+function Afip(options = {}) {
+  /**
+   * File name for the WSDL corresponding to WSAA
+   *
+   * @var string
+   **/
+  this.WSAA_WSDL;
 
-	/**
-	 * The url to get WSAA token
-	 *
-	 * @var string
-	 **/
-	this.WSAA_URL;
+  /**
+   * The url to get WSAA token
+   *
+   * @var string
+   **/
+  this.WSAA_URL;
 
-	/**
-	 * File name for the X.509 certificate in PEM format
-	 *
-	 * @var string
-	 **/
-	this.CERT;
+  /**
+   * File name for the X.509 certificate in PEM format
+   *
+   * @var string
+   **/
+  this.CERT;
 
-	/**
-	 * File name for the private key correspoding to CERT (PEM)
-	 *
-	 * @var string
-	 **/
-	this.PRIVATEKEY;
+  /**
+   * File name for the private key correspoding to CERT (PEM)
+   *
+   * @var string
+   **/
+  this.PRIVATEKEY;
 
-	/**
-	 * Afip resources folder
-	 *
-	 * @var string
-	 **/
-	this.RES_FOLDER;
+  /**
+   * Afip resources folder
+   *
+   * @var string
+   **/
+  this.RES_FOLDER;
 
-	/**
-	 * The CUIT to use
-	 *
-	 * @var int
-	 **/
-	this.CUIT;
+  /**
+   * The CUIT to use
+   *
+   * @var int
+   **/
+  this.CUIT;
 
-	// Create an Afip instance if it is not
-	if (!(this instanceof Afip)) {return new Afip(options)}
+  // Create an Afip instance if it is not
+  if (!(this instanceof Afip)) {
+    return new Afip(options);
+  }
 
+  if (!options.hasOwnProperty("CUIT")) {
+    throw new Error("CUIT field is required in options array");
+  }
 
-	if (!options.hasOwnProperty('CUIT')) {throw new Error("CUIT field is required in options array");}
-	
-	// Define default options
-	if (!options.hasOwnProperty('production')) {options['production'] = false;}
-	if (!options.hasOwnProperty('cert')) {options['cert'] = 'cert';}
-	if (!options.hasOwnProperty('key')) {options['key'] = 'key';}
-	if (!options.hasOwnProperty('res_folder')) {options['res_folder'] = __dirname+'/Afip_res/';}
-	if (!options.hasOwnProperty('ta_folder')) {options['ta_folder'] = __dirname+'/Afip_res/';}
-	if (options['production'] !== true) {options['production'] = false;}
+  // Define default options
+  if (!options.hasOwnProperty("production")) {
+    options["production"] = false;
+  }
+  if (!options.hasOwnProperty("cert")) {
+    options["cert"] = "cert";
+  }
+  if (!options.hasOwnProperty("key")) {
+    options["key"] = "key";
+  }
+  if (!options.hasOwnProperty("res_folder")) {
+    options["res_folder"] = __dirname + "/Afip_res/";
+  }
+  if (!options.hasOwnProperty("ta_folder")) {
+    options["ta_folder"] = __dirname + "/Afip_res/";
+  }
+  if (options["production"] !== true) {
+    options["production"] = false;
+  }
 
-	this.options = options;
+  this.options = options;
 
-	this.CUIT 		= options['CUIT'];
-	this.RES_FOLDER = options['res_folder'];
-	this.TA_FOLDER 	= options['ta_folder'];
-	this.CERT 		= path.resolve(this.RES_FOLDER, options['cert']);
-	this.PRIVATEKEY = path.resolve(this.RES_FOLDER, options['key']);
-	this.WSAA_WSDL 	= path.resolve(__dirname, 'Afip_res/', 'wsaa.wsdl');
+  this.CUIT = options["CUIT"];
+  this.RES_FOLDER = options["res_folder"];
+  this.TA_FOLDER = options["ta_folder"];
+  this.CERT = path.resolve(this.RES_FOLDER, options["cert"]);
+  this.PRIVATEKEY = path.resolve(this.RES_FOLDER, options["key"]);
+  this.WSAA_WSDL = path.resolve(__dirname, "Afip_res/", "wsaa.wsdl");
 
-	if (options['production']) {
-		this.WSAA_URL = 'https://wsaa.afip.gov.ar/ws/services/LoginCms';
-	}
-	else {
-		this.WSAA_URL = 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms';
-	}
+  if (options["production"]) {
+    this.WSAA_URL = "https://wsaa.afip.gov.ar/ws/services/LoginCms";
+  } else {
+    this.WSAA_URL = "https://wsaahomo.afip.gov.ar/ws/services/LoginCms";
+  }
 
-	this.ElectronicBilling 			= new ElectronicBilling(this);
-	this.RegisterScopeFour 			= new RegisterScopeFour(this);
-	this.RegisterScopeFive 			= new RegisterScopeFive(this);
-	this.RegisterInscriptionProof 	= new RegisterScopeFive(this);
-	this.RegisterScopeTen 			= new RegisterScopeTen(this);
-	this.RegisterScopeThirteen 		= new RegisterScopeThirteen(this);
+  this.ElectronicBilling = new ElectronicBilling(this);
+  this.ElectronicExportBilling = new ElectronicExportBilling(this);
 }
 
 /**
@@ -125,151 +131,170 @@ function Afip(options = {}){
  *
  * @param service Service for token authorization
  **/
-Afip.prototype.GetServiceTA = async function(service, firstTry = true) {
-	// Declare token authorization file path
-	const taFilePath = path.resolve(
-		this.TA_FOLDER,
-		`TA-${this.options['CUIT']}-${service}${this.options['production'] ? '-production' : ''}.json`
-	);
+Afip.prototype.GetServiceTA = async function (service, firstTry = true) {
+  // Declare token authorization file path
+  const taFilePath = path.resolve(
+    this.TA_FOLDER,
+    `TA-${this.options["CUIT"]}-${service}${
+      this.options["production"] ? "-production" : ""
+    }.json`
+  );
 
-	// Check if token authorization file exists
-	const taFileAccessError = await new Promise((resolve) => {
-		fs.access(taFilePath, fs.constants.F_OK, resolve);
-	}); 
+  // Check if token authorization file exists
+  const taFileAccessError = await new Promise((resolve) => {
+    fs.access(taFilePath, fs.constants.F_OK, resolve);
+  });
 
-	// If have access to token authorization file
-	if (!taFileAccessError) {
-		const taData = require(taFilePath);
-		const actualTime = new Date(Date.now() + 600000);
-		const expirationTime = new Date(taData.header[1].expirationtime);
+  // If have access to token authorization file
+  if (!taFileAccessError) {
+    const taData = require(taFilePath);
+    const actualTime = new Date(Date.now() + 600000);
+    const expirationTime = new Date(taData.header[1].expirationtime);
 
-		// Delete TA cache
-		delete require.cache[require.resolve(taFilePath)];
+    // Delete TA cache
+    delete require.cache[require.resolve(taFilePath)];
 
-		if (actualTime < expirationTime) {
-			// Return token authorization
-			return {
-				token : taData.credentials.token,
-				sign : taData.credentials.sign
-			}
-		}
-	}
-	
-	// Throw error if this is not the first try to get token authorization
-	if (firstTry === false){
-		throw new Error('Error getting Token Autorization');
-	}
+    if (actualTime < expirationTime) {
+      // Return token authorization
+      return {
+        token: taData.credentials.token,
+        sign: taData.credentials.sign,
+      };
+    }
+  }
 
-	// Create token authorization file
-	await this.CreateServiceTA(service).catch(err => {
-		throw new Error(`Error getting Token Autorization ${err}`)
-	});
+  // Throw error if this is not the first try to get token authorization
+  if (firstTry === false) {
+    throw new Error("Error getting Token Autorization");
+  }
 
-	// Try to get token authorization one more time
-	return await this.GetServiceTA(service, false);
-}
+  // Create token authorization file
+  await this.CreateServiceTA(service).catch((err) => {
+    throw new Error(`Error getting Token Autorization ${err}`);
+  });
+
+  // Try to get token authorization one more time
+  return await this.GetServiceTA(service, false);
+};
 
 /**
  * Create an TA from WSAA
  *
- * Request to WSAA for a tokent authorization for service 
+ * Request to WSAA for a tokent authorization for service
  * and save this in a json file
  *
  * @param service Service for token authorization
  **/
-Afip.prototype.CreateServiceTA = async function(service) {
-	const date = new Date();
-	
-	// Tokent request authorization XML
-	const tra = (`<?xml version="1.0" encoding="UTF-8" ?>
+Afip.prototype.CreateServiceTA = async function (service) {
+  const date = new Date();
+
+  // Tokent request authorization XML
+  const tra = `<?xml version="1.0" encoding="UTF-8" ?>
 	<loginTicketRequest version="1.0">
 		<header>
 			<uniqueId>${Math.floor(date.getTime() / 1000)}</uniqueId>
-			<generationTime>${new Date(date.getTime() - 600000).toISOString()}</generationTime>
-			<expirationTime>${new Date(date.getTime() + 600000).toISOString()}</expirationTime>
+			<generationTime>${new Date(
+        date.getTime() - 600000
+      ).toISOString()}</generationTime>
+			<expirationTime>${new Date(
+        date.getTime() + 600000
+      ).toISOString()}</expirationTime>
 		</header>
 		<service>${service}</service>
-	</loginTicketRequest>`).trim();
+	</loginTicketRequest>`.trim();
 
-	// Get cert file content
-	const certPromise = new Promise((resolve, reject) => {
-		fs.readFile(this.CERT, { encoding:'utf8' }, (err, data) => err ? reject(err) : resolve(data));
-	});
-		
-	// Get key file content
-	const keyPromise = new Promise((resolve, reject) => {
-		fs.readFile(this.PRIVATEKEY, { encoding:'utf8' }, (err, data) => err ? reject(err) : resolve(data));
-	});
+  // Get cert file content
+  const certPromise = new Promise((resolve, reject) => {
+    fs.readFile(this.CERT, { encoding: "utf8" }, (err, data) =>
+      err ? reject(err) : resolve(data)
+    );
+  });
 
-	// Wait for cert and key content
-	const [cert, key] = await Promise.all([certPromise, keyPromise]);
+  // Get key file content
+  const keyPromise = new Promise((resolve, reject) => {
+    fs.readFile(this.PRIVATEKEY, { encoding: "utf8" }, (err, data) =>
+      err ? reject(err) : resolve(data)
+    );
+  });
 
-	// Sign Tokent request authorization XML
-	const p7 = forge.pkcs7.createSignedData();
-	p7.content = forge.util.createBuffer(tra, "utf8");
-	p7.addCertificate(cert);
-	p7.addSigner({
-		authenticatedAttributes: [{
-			type: forge.pki.oids.contentType,
-			value: forge.pki.oids.data,
-		}, 
-		{
-			type: forge.pki.oids.messageDigest
-		}, 
-		{
-			type: forge.pki.oids.signingTime, 
-			value: new Date()
-		}],
-		certificate: cert,
-		digestAlgorithm: forge.pki.oids.sha256,
-		key: key,
-	});
-	p7.sign();
-	const bytes = forge.asn1.toDer(p7.toAsn1()).getBytes();
-	const signedTRA = Buffer.from(bytes, "binary").toString("base64");
+  // Wait for cert and key content
+  const [cert, key] = await Promise.all([certPromise, keyPromise]);
 
-	// SOAP Client options
-	const soapClientOptions = { disableCache:true, endpoint: this.WSAA_URL };
+  // Sign Tokent request authorization XML
+  const p7 = forge.pkcs7.createSignedData();
+  p7.content = forge.util.createBuffer(tra, "utf8");
+  p7.addCertificate(cert);
+  p7.addSigner({
+    authenticatedAttributes: [
+      {
+        type: forge.pki.oids.contentType,
+        value: forge.pki.oids.data,
+      },
+      {
+        type: forge.pki.oids.messageDigest,
+      },
+      {
+        type: forge.pki.oids.signingTime,
+        value: new Date(),
+      },
+    ],
+    certificate: cert,
+    digestAlgorithm: forge.pki.oids.sha256,
+    key: key,
+  });
+  p7.sign();
+  const bytes = forge.asn1.toDer(p7.toAsn1()).getBytes();
+  const signedTRA = Buffer.from(bytes, "binary").toString("base64");
 
-	// Create SOAP client
-	const soapClient = await soap.createClientAsync(this.WSAA_WSDL, soapClientOptions);
+  // SOAP Client options
+  const soapClientOptions = { disableCache: true, endpoint: this.WSAA_URL };
 
-	// Arguments for soap client request 
-	const loginArguments = { in0: signedTRA };
-	
-	// Call loginCms SOAP method
-	const [ loginCmsResult ] = await soapClient.loginCmsAsync(loginArguments)
+  // Create SOAP client
+  const soapClient = await soap.createClientAsync(
+    this.WSAA_WSDL,
+    soapClientOptions
+  );
 
-	// Parse loginCmsReturn to JSON 
-	const res = await xmlParser.parseStringPromise(loginCmsResult.loginCmsReturn); 
+  // Arguments for soap client request
+  const loginArguments = { in0: signedTRA };
 
-	// Declare token authorization file path
-	const taFilePath = path.resolve(
-		this.TA_FOLDER,
-		`TA-${this.options['CUIT']}-${service}${this.options['production'] ? '-production' : ''}.json`
-	);
-	
-	// Save Token authorization data to json file
-	await (new Promise((resolve, reject) => {
-		fs.writeFile(taFilePath, JSON.stringify(res.loginticketresponse), (err) => {
-			if (err) {reject(err);return;}
-			resolve();
-		});
-	}));
-}
+  // Call loginCms SOAP method
+  const [loginCmsResult] = await soapClient.loginCmsAsync(loginArguments);
+
+  // Parse loginCmsReturn to JSON
+  const res = await xmlParser.parseStringPromise(loginCmsResult.loginCmsReturn);
+
+  // Declare token authorization file path
+  const taFilePath = path.resolve(
+    this.TA_FOLDER,
+    `TA-${this.options["CUIT"]}-${service}${
+      this.options["production"] ? "-production" : ""
+    }.json`
+  );
+
+  // Save Token authorization data to json file
+  await new Promise((resolve, reject) => {
+    fs.writeFile(taFilePath, JSON.stringify(res.loginticketresponse), (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+};
 
 /**
  * Create generic Web Service
- * 
+ *
  * @param string service Web Service name
  * @param array options Web Service options
  *
- * @return AfipWebService Token Authorization for AFIP Web Service 
+ * @return AfipWebService Token Authorization for AFIP Web Service
  **/
-Afip.prototype.WebService = function (service, options)
-{
-	options['service'] = service;
-	options['generic'] = true;
+Afip.prototype.WebService = function (service, options) {
+  options["service"] = service;
+  options["generic"] = true;
 
-	return new AfipWebService({ afip: this }, options);
-}
+  return new AfipWebService({ afip: this }, options);
+};
---
+++ src/Class/ElectronicExportBilling.js
@@ -0,0 +1,389 @@
+const AfipWebService = require('./AfipWebService');
+
+/**
+ * SDK for AFIP Electronic Billing (WSFEX)
+ * 
+ * @link https://www.afip.gob.ar/fe/documentos/WSFEX-Manualparaeldesarrollador_V1_9.pdf WS Specification
+ **/
+module.exports = class ElectronicExportBilling extends AfipWebService {
+	constructor(afip){
+		const options = {
+			soapV12: true,
+			WSDL: 'wsfex-production.wsdl',
+			URL: 'https://servicios1.afip.gov.ar/wsfexv1/service.asmx',
+			WSDL_TEST: 'wsfex.wsdl',
+			URL_TEST: 'https://wswhomo.afip.gov.ar/wsfexv1/service.asmx',
+			afip
+		}
+ 
+		super(options);
+	}
+
+	/**
+	 * Gets last voucher number 
+	 * 
+	 * Asks to Afip servers for number of the last voucher created for
+	 * certain sales point and voucher type {@see WS Specification 
+	 * item 2.4} 
+	 *
+	 * @param int salesPoint 	Sales point to ask for last voucher  
+	 * @param int type 		Voucher type to ask for last voucher 
+	 *
+	 * @return int 
+	 **/
+	async getLastVoucher(salesPoint, type) {
+		const req = {
+			'Pto_venta' 	: salesPoint,
+			'Cbte_Tipo' 	: type
+		};
+
+		return (await this.executeRequest('FEXGetLast_CMP', req)).Cbte_nro;
+	}
+
+	/**
+	 * Create a voucher from AFIP
+	 *
+	 * Send to AFIP servers request for create a voucher and assign 
+	 * CAE to them {@see WS Specification item 4.1}
+	 * 
+	 * @param array data Voucher parameters {@see WS Specification 
+	 * 	item 4.1.3}, some arrays were simplified for easy use {@example 
+	 * 	examples/createVoucher.js Example with all allowed
+	 * 	 attributes}
+	 * @param bool returnResponse if is TRUE returns complete response  
+	 * 	from AFIP
+	 * 
+	 * @return array if returnResponse is set to false returns 
+	 * 	[CAE : CAE assigned to voucher, CAEFchVto : Expiration date 
+	 * 	for CAE (yyyy-mm-dd)] else returns complete response from 
+	 * 	AFIP {@see WS Specification item 4.1.3}
+	 **/
+	async createVoucher(data, returnResponse = false) {
+		const Id = await this.getLastId()
+		const req = {
+			'Cmp' : {
+				'Id': Id,
+				...data,
+			}
+		};
+ 
+
+		if (data['Permisos']) 
+			data['Permisos'] = { 'Permiso' : data['Permisos'] };
+
+		if (data['Cmps_asoc']) 
+			data['Cmps_asoc'] = { 'Cmp_asoc' : data['Cmps_asoc'] };
+		
+		if (data['Items']) 
+			data['Items'] = { 'Item' : data['Items'] };
+		
+		if (data['Opcionales']) 
+			data['Opcionales'] = { 'Opcional' : data['Opcionales'] };
+
+		const results = await this.executeRequest('FEXAuthorize', req);
+
+		if (returnResponse === true) {
+			return results;
+		}
+		else{
+			if (Array.isArray(results.FEXAuthorizeResponse.FEXResultAuth)) {
+				results.FEXAuthorizeResponse.FEXResultAuth = results.FEXAuthorizeResponse.FEXResultAuth[0];
+			}
+
+			return {
+				'CAE' 		: results.FEXAuthorizeResponse.FEXResultAuth.Cae,
+				'CAEFchVto' : this.formatDate(results.FEXAuthorizeResponse.FEXResultAuth.Fch_venc_Cae),
+			};
+		}
+	}
+
+	/**
+	 * Create next voucher from AFIP
+	 *
+	 * This method combines Afip.getLastVoucher and Afip.createVoucher
+	 * for create the next voucher
+	 *
+	 * @param array data Same to data in Afip.createVoucher except that
+	 * 	don't need CbteDesde and CbteHasta attributes
+	 *
+	 * @return array [CAE : CAE assigned to voucher, CAEFchVto : Expiration 
+	 * 	date for CAE (yyyy-mm-dd), voucherNumber : Number assigned to 
+	 * 	voucher]
+	 **/
+	async createNextVoucher(data) {
+		const lastVoucher = await this.getLastVoucher(data['PtoVta'], data['CbteTipo']);
+		
+		const voucherNumber = lastVoucher + 1;
+
+		data['CbteDesde'] = voucherNumber;
+		data['CbteHasta'] = voucherNumber;
+
+		let res 				= await this.createVoucher(data);
+		res['voucherNumber'] 	= voucherNumber;
+
+		return res;
+	}
+ 
+	/**
+	 * Get complete voucher information
+	 *
+	 * Asks to AFIP servers for complete information of voucher {@see WS 
+	 * Specification item 2.2 }
+	 *
+	 * @param int number 		Number of voucher to get information
+	 * @param int salesPoint 	Sales point of voucher to get information
+	 * @param int type 			Type of voucher to get information
+	 *
+	 * @return array|null returns array with complete voucher information 
+	 * 	{@see WS Specification item 2.2 } or null if there not exists 
+	 **/
+	 async getVoucherInfo(number, salesPoint, type) {
+		const req = {
+			'Cmp' : {
+				'Cbte_nro' 	: number,
+				'Punto_vta' 	: salesPoint,
+				'Cbte_Tipo' 	: type
+			}
+		};
+
+		const result = await this.executeRequest('FEXGetCMP', req)
+		.catch(err => { if (err.code === 1020) { return null } else { throw err }});
+
+		return result.ResultGet;
+	}
+	
+	/**
+	 * Asks to AFIP Servers for requirement id availables {@see WS 
+	 * Specification item 2.3}
+	 *
+	 * @return long id
+	 **/
+	async getLastId() {
+		return (await this.executeRequest('FEXGetLast_ID')).FEXResultGet.Id;
+	}
+
+	/**
+	 * Asks to AFIP Servers for all currencies availables {@see WS 
+	 * Specification item 2.5}
+	 *
+	 * @return array all currencies
+	 **/
+	 async getCurrencies() {
+		return (await this.executeRequest('FEXGetPARAM_MON')).FEXResultGet.ClsFEXResponse_Mon;
+	}
+
+	/**
+	 * Asks to AFIP Servers for all exports types availables {@see WS 
+	 * Specification item 2.7}
+	 *
+	 * @return array all exports type
+	 **/
+	 async getExportTypes() {
+		return (await this.executeRequest('ClsFEXResponse_Tex')).FEXResultGet.ClsFEXResponse_Mon;
+	}
+	
+	/**
+	 * Asks to AFIP Servers for all unit measures availables {@see WS 
+	 * Specification item 2.8}
+	 *
+	 * @return array all units measures type
+	 **/
+	 async getUnits() {
+		return (await this.executeRequest('FEXGetPARAM_UMed')).FEXResultGet.ClsFEXResponse_UMed;
+	}
+
+	/**
+	 * Asks to AFIP Servers for languagues availables {@see WS 
+	 * Specification item 2.9}
+	 *
+	 * @return array all language  
+	 **/
+	 async getLanguage() {
+		return (await this.executeRequest('FEXGetPARAM_Idiomas')).FEXResultGet.ClsFEXResponse_Idi;
+	}
+
+	/**
+	 * Asks to AFIP Servers for countries availables {@see WS 
+	 * Specification item 2.10}
+	 *
+	 * @return array all countries  
+	 **/
+		async getCountries() {
+		return (await this.executeRequest('FEXGetPARAM_DST_pais')).FEXResultGet.ClsFEXResponse_DST_pais;
+	}
+	
+	/**
+	 * Asks to AFIP Servers for incoterms {@see WS 
+	 * Specification item 2.11}
+	 *
+	 * @return array all Incoterms  
+	 **/
+	 async getIncoterms() {
+		return (await this.executeRequest('FEXGetPARAM_Incoterms')).FEXResultGet.ClsFEXResponse_Inc;
+	}
+
+	/**
+	 * Asks to AFIP Servers for CUIT countries availables {@see WS 
+	 * Specification item 2.12}
+	 *
+	 * @return array all CUIT countries  
+	 **/
+	async getCUITsOfCountries() {
+		return (await this.executeRequest('FEXGetPARAM_DST_CUIT')).FEXResultGet.ClsFEXResponse_DST_cuit;
+	}
+ 
+	/**
+	 * Get complete voucher information
+	 *
+	 * Asks to AFIP servers for complete information of voucher {@see WS 
+	 * Specification item 2.13 }
+	 *
+	 * @param moneyId string 	moneyId to get information
+	 *
+	 * @return array|null returns array with complete voucher information 
+	 * 	{@see WS Specification item 2.13 } or null if there not exists 
+	 **/
+	 async getQuoteCurrencyByMoneyId(moneyId) {
+		const req = {
+			'Mon_id' : moneyId
+		};
+
+		const result = await this.executeRequest('FEXGetPARAM_Ctz', req)
+		.catch(err => {   throw err });
+
+		return result.ResultGet;
+	}
+
+	/**
+	 * Asks to AFIP Servers for CUIT countries availables {@see WS 
+	 * Specification item 2.14}
+	 *
+	 * @return array all sales points countries  
+	 **/
+	 async getSalesPointsValids() {
+		return (await this.executeRequest('FEXGetPARAM_PtoVenta')).FEXResultGet.ClsFEXResponse_PtoVenta;
+	}
+
+	/**
+	 * Asks to AFIP Servers for CUIT countries availables {@see WS 
+	 * Specification item 2.15}
+	 *
+	 * @return array all options types  
+	 **/
+	async getOptionsTypes() {
+		return (await this.executeRequest('FEXGetPARAM_Opcionales')).FEXResultGet.ClsFEXResponse_Opc;
+	}
+
+	/**
+	 * Get complete voucher information
+	 *
+	 * Asks to AFIP servers for complete information of voucher {@see WS 
+	 * Specification item 2.13 }
+	 *
+	 * @param permissionId string 	moneyId to get information
+	 * @param countryid int countryid to get information
+	 *
+	 * @return array|null returns array with complete voucher information 
+	 * 	{@see WS Specification item 2.13 } or null if there not exists 
+	 **/
+	 async verifyPermissionExistenceCountryById(permissionId, countryId) {
+		const req = {
+			'ID_Permiso' : permissionId,
+			'Dst_merc' : countryId
+		};
+
+		const result = await this.executeRequest('FEXCheck_Permiso', req)
+		.catch(err => {   throw err });
+
+		return result.ResultGet;
+	}
+
+	/**
+	 * Asks to web service for servers status {@see WS 
+	 * Specification item 2.17}
+	 *
+	 * @return object { AppServer : Web Service status, 
+	 * DbServer : Database status, AuthServer : Autentication 
+	 * server status}
+	 **/
+	async getServerStatus() {
+		return await this.executeRequest('FEXDummy');
+	}
+
+	/**
+	 * Change date from AFIP used format (yyyymmdd) to yyyy-mm-dd
+	 *
+	 * @param string|int date to format
+	 *
+	 * @return string date in format yyyy-mm-dd
+	 **/
+	formatDate(date) {
+		return date.toString()
+		.replace(/(\d{4})(\d{2})(\d{2})/, (string, year, month, day) => `${year}-${month}-${day}`);
+	}
+
+	/**
+	 * Sends request to AFIP servers
+	 * 
+	 * @param string 	operation 	SOAP operation to do 
+	 * @param array 	params 	Parameters to send
+	 *
+	 * @return mixed Operation results 
+	 **/
+	async executeRequest(operation, params = {})
+	{
+		Object.assign(params, await this.getWSInitialRequest(operation)); 
+
+		const results = await super.executeRequest(operation, params);
+
+		await this._checkErrors(operation, results);
+
+		return results[operation+'Result'];
+	}
+
+	/**
+	 * Make default request parameters for most of the operations
+	 * 
+	 * @param string operation SOAP Operation to do 
+	 *
+	 * @return array Request parameters  
+	 **/
+	async getWSInitialRequest(operation)
+	{
+		if (operation === 'FEDummy') {
+			return {};
+		}
+
+		const { token, sign } = await this.afip.GetServiceTA('wsfex');
+
+		return {
+			'Auth' : { 
+				'Token' : token,
+				'Sign' 	: sign,
+				'Cuit' 	: this.afip.CUIT
+				}
+		};
+	}
+
+	/**
+	 * Check if occurs an error on Web Service request
+	 * 
+	 * @param string 	operation 	SOAP operation to check 
+	 * @param mixed 	results 	AFIP response
+	 *
+	 * @throws Exception if exists an error in response 
+	 * 
+	 * @return void 
+	 **/
+	async _checkErrors(operation, results)
+	{
+		const res = results[operation+'Result'];
+
+		if (res.FEXErr) {
+			const err = Array.isArray(res.FEXErr) ? res.FEXErr[0] : res.FEXErr;
+			throw new Error(`(${err.ErrCode}) ${err.ErrMsg}`, err.ErrCode);
+		}
+	}
+
+}
+
